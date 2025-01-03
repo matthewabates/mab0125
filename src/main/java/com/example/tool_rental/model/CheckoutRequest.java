da49fa3d8f2ba9cd1e1f209a7db2e616582be5c0
matthewabates <matthew.a.bates@gmail.com>
2025-01-03 15:21:17 -0600

    add helpful comments from specification document

--- src/main/java/com/example/tool_rental/model/CheckoutRequest.java
+++ src/main/java/com/example/tool_rental/model/CheckoutRequest.java
@@ -12,6 +12,7 @@ public class CheckoutRequest {
     @NotBlank
     String toolCode;
 
+    //The number of days for which the customer wants to rent the tool. (e.g. 4 days)
     @NotNull
     @Min(1)
     Integer rentalDayCount;
@@ -19,6 +20,7 @@ public class CheckoutRequest {
     @NotNull
     @Min(0)
     @Max(100)
+    //As a whole number, 0-100 (e.g. 20 = 20%)
     Integer discountPercent;
 
     @NotNull
--- src/main/java/com/example/tool_rental/model/RentalAgreement.java
+++ src/main/java/com/example/tool_rental/model/RentalAgreement.java
@@ -2,19 +2,32 @@ package com.example.tool_rental.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.util.Currency;
 import java.util.Date;
 
 @Getter
 @Setter
 public class RentalAgreement {
-    Tool tool;
-    CheckoutRequest checkoutRequest;
-    Date duedate;
-    float dailyRentalCharge;
-    int chargeDays;
-    float preDiscountCharge;
-    float discountAmount;
-    float finalCharge;
+
+//    Tool tool;
+    String toolCode; //Specified at checkout
+    String toolType; //From tool info
+    String toolBrand; //From tool info
+    Currency dailyRentalCharge; //Amount per day, specified by the tool type.
+
+//    CheckoutRequest checkoutRequest;
+    Integer rentalDays; //Specified at checkout
+    Date checkoutDate; //Specified at checkout
+    Integer discountPercent; //Specified at checkout.
+
+    @DateTimeFormat(pattern = "MM/dd/yy")
+    Date dueDate; //Calculated from checkout date and rental days.
+    Integer chargeDays; //Count of chargeable days, from day after checkout through and including due date, excluding “no charge” days as specified by the tool type.
+    Currency preDiscountCharge; //Calculated as charge days X daily charge. Resulting total rounded half up to cents.
+    Currency discountAmount; //calculated from discount % and pre-discount charge. Resulting amount rounded half up to cents.
+    Currency finalCharge; //Calculated as pre-discount charge - discount amount.
+
     //TODO override tostring or some other print method
 }
