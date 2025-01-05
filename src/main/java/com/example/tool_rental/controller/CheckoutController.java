8b4e5a27c5ec38555a7fc968d52629214b9ffb6b
matthewabates <matthew.a.bates@gmail.com>
2025-01-04 23:20:36 -0600

    checkout service now looks up tools from the database and the checkout controller responds with useful 404 messages when tool is not found

--- src/main/java/com/example/tool_rental/controller/CheckoutController.java
+++ src/main/java/com/example/tool_rental/controller/CheckoutController.java
@@ -3,9 +3,11 @@ package com.example.tool_rental.controller;
 import com.example.tool_rental.model.CheckoutRequest;
 import com.example.tool_rental.model.RentalAgreement;
 import com.example.tool_rental.service.CheckoutService;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
@@ -26,7 +28,7 @@ public class CheckoutController {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
@@ -34,5 +36,13 @@ public class CheckoutController {
             errors.put(fieldName, errorMessage);
         });
         return errors;
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleEntityNotFoundException(EntityNotFoundException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", "Resource Not Found");
+        response.put("message", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/example/tool_rental/service/CheckoutService.java
+++ src/main/java/com/example/tool_rental/service/CheckoutService.java
@@ -3,18 +3,23 @@ package com.example.tool_rental.service;
 import com.example.tool_rental.model.CheckoutRequest;
 import com.example.tool_rental.model.RentalAgreement;
 import com.example.tool_rental.model.Tool;
-import com.example.tool_rental.model.ToolType;
+import com.example.tool_rental.repo.ToolRepository;
 import com.example.tool_rental.util.CostUtil;
 import com.example.tool_rental.util.DateUtil;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /*
     Responsible for generating completed RentalAgreements from CheckoutRequests.
-    Uses external dependencies to calculate charge days, cost, discount, final cost etc
+    Looks up tool info from database,
+    Uses external Cost util to calculate charge days, cost, discount, final cost etc
  */
 @Service
 public class CheckoutService {
+
+    @Autowired
+    ToolRepository toolRepository;
 
     @Autowired
     CostUtil costUtil;
@@ -23,16 +28,14 @@ public class CheckoutService {
     DateUtil dateUtil;
 
     public RentalAgreement checkout(CheckoutRequest checkoutRequest) {
-        Tool tool = new Tool(); //TODO look up from repo, throw exception that generates a good response
-        ToolType toolType = tool.getToolType(); //TODO look up from repo, throw exception that generates a good response
-
+        Tool tool = getTool(checkoutRequest);
         RentalAgreement rentalAgreement = new RentalAgreement();
 
         //from tool info
         rentalAgreement.setToolCode(tool.getToolCode());
-        rentalAgreement.setToolType(toolType.getName());
+        rentalAgreement.setToolType(tool.getToolType().getName());
         rentalAgreement.setToolBrand(tool.getBrand());
-        rentalAgreement.setDailyRentalCharge(toolType.getDailyCharge());
+        rentalAgreement.setDailyRentalCharge(tool.getToolType().getDailyCharge());
 
         //from checkout request
         rentalAgreement.setRentalDays(checkoutRequest.getRentalDayCount());
@@ -41,11 +44,17 @@ public class CheckoutService {
 
         //calculated values
         rentalAgreement.setDueDate(dateUtil.calculateDueDate(rentalAgreement.getCheckoutDate(), rentalAgreement.getRentalDays()));
-        rentalAgreement.setChargeDays(costUtil.calculateChargeDays(rentalAgreement, toolType));
+        rentalAgreement.setChargeDays(costUtil.calculateChargeDays(rentalAgreement, tool.getToolType()));
         rentalAgreement.setPreDiscountCharge(costUtil.calculatePreDiscountCharge(rentalAgreement));
         rentalAgreement.setDiscountAmount(costUtil.calculateDiscountAmount(rentalAgreement));
         rentalAgreement.setFinalCharge(costUtil.calculateFinalCharge(rentalAgreement));
 
         return rentalAgreement;
     }
+
+    private Tool getTool(CheckoutRequest checkoutRequest) {
+        return toolRepository.findByToolCode(checkoutRequest.getToolCode())
+                .orElseThrow(() -> new EntityNotFoundException("Tool with code " + checkoutRequest.getToolCode() + " not found."));
+    }
+
 }
